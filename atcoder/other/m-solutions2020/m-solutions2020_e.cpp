ac0eb8d83d96be89f1da72f9d303fff58ae70fba
HecklerUK <taiki.namiki@gmail.com>
2020-07-30 00:22:15 +0900

    add m-solution2020 e

--- atcoder/other/m-solutions2020/m-solutions2020_e.cpp
+++ atcoder/other/m-solutions2020/m-solutions2020_e.cpp
@@ -38,9 +38,70 @@ const int MOD = 1e9 + 7;
 const int dx[4] = {-1, 0, 1, 0};
 const int dy[4] = {0, -1, 0, 1};
 
+
+vll call(vpl h){
+  ll n=h.size();
+  sort(ALL(h));
+
+  vll res(n+1);
+
+  vvll dp(20,vll(20,LLINF));
+  dp[0][0]=0;
+  REP(l,n){
+    for(ll r=l+1; r<=n; r++){
+      ll now=LLINF;
+      for(ll k=l; k<r; k++){
+        ll s=0;
+        for(ll i=l; i<r; i++)
+          s+=abs(h[i].FI-h[k].FI)*h[i].SE;
+        now=min(now,s);
+      }
+
+
+      REP(j,n)
+        dp[r][j+1]=min(dp[l][j]+now,dp[r][j+1]);
+
+
+      //axis check
+      now=0;
+      for(ll i=l; i<r; i++)
+        now+=abs(h[i].FI)*h[i].SE;
+      REP(j,n)
+        dp[r][j]=min(dp[l][j]+now,dp[r][j]);
+    }
+  }
+
+
+  REP(i,n+1)res[i]=dp[n][i];
+  return res;
+}
+
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
+
+  ll n;
+  cin>>n;
+  vll x(n),y(n),p(n);
+  REP(i,n)cin>>x[i]>>y[i]>>p[i];
+
+  vll ans(2*n+1,LLINF);
+  REP(i,(1<<n)){
+    vpl ax,ay;
+    REP(j,n){
+      if((i>>j)&1)
+        ax.push_back(pll(x[j],p[j]));
+      else
+        ay.push_back(pll(y[j],p[j]));
+    }
+
+    vll rx=call(ax);
+    vll ry=call(ay);
+
+    REP(j,rx.size())REP(k,ry.size())ans[j+k]=min(ans[j+k],rx[j]+ry[k]);
+  }
+
+  REP(i,n+1)cout<<ans[i]<<endl;
 
 }
 
