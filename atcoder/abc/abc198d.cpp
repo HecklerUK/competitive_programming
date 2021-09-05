067852a4f5a337232df1e3911f35cea976c54f41
HecklerUK <taiki.namiki@gmail.com>
2021-09-05 23:41:33 +0900

    fix abc198d,e

--- atcoder/abc/abc198d.cpp
+++ atcoder/abc/abc198d.cpp
@@ -38,32 +38,6 @@ const int MOD = 1e9 + 7;
 const int dx[4] = {-1, 0, 1, 0};
 const int dy[4] = {0, -1, 0, 1};
 
-bool flag=false;
-vll un(10);
-ll CNT=0;
-
-void dfs_pick(vll& indexes, int rest, std::function<void(vll)> f) {
-  if (rest == 0){
-    CNT++;
-    return f(indexes);
-  }
-  else {
-    REP(i,10){
-      if(un[i]!=0)
-        continue;
-      indexes[rest-1] = i;
-      un[i]++;
-      dfs_pick(indexes, rest-1, f);
-      un[i]--;
-    }
-  }
-}
-
-void run(ll n, ll k, std::function<void(vll)> f) {
-  vll indexes(k);
-  dfs_pick(indexes, k, f);
-}
-
 
 int main(){
   ios_base::sync_with_stdio(false);
@@ -81,35 +55,40 @@ int main(){
 
   map<char,int> table;
   vll mp(256);
+  vll p(10);
+  REP(i,10)p[i]=i;
+  bool flag=false;
 
-  auto solve=[&](vll p){
+  do{
     REP(i,s.size())mp[s[i]]=p[i];
 
     if(flag)
-      return;
-    if(p[s1[0]]==0 || p[s2[0]]==0 || p[s3[0]]==0)
-      return;
+      continue;
+    if(mp[s1[0]]==0)
+      continue;
+    if(mp[s2[0]]==0)
+      continue;
+    if(mp[s3[0]]==0)
+      continue;
 
-    vll t(3);
-    int n1=0,n2=0,n3=0;
-    REP(i,s1.size())n1=n1*10+p[s1[i]];
-    REP(i,s2.size())n2=n2*10+p[s2[i]];
-    REP(i,s3.size())n3=n3*10+p[s3[i]];
+    ll n1=0,n2=0,n3=0;
+    REP(i,s1.size())n1=n1*10+mp[s1[i]];
+    REP(i,s2.size())n2=n2*10+mp[s2[i]];
+    REP(i,s3.size())n3=n3*10+mp[s3[i]];
 
     if(n1+n2!=n3)
-      return;
+      continue;
 
     cout<<n1<<endl;
     cout<<n2<<endl;
     cout<<n3<<endl;
 
     flag=true;
-  };
+  }
+  while(next_permutation(ALL(p)));
 
-  run(10,s.size(),solve);
   if(!flag)
     cout<<"UNSOLVABLE"<<endl;
 
-  cout<<CNT<<endl;
 }
 
