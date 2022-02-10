cef427017c9fdb67077be8b42f5792204d6d4892
kochheckler <lucky77ducky@gmali.com>
2022-02-10 23:57:52 +0900

    solve abc236 first time

---
+++ atcoder/abc/abc236d.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+
+#define all(a) a.begin(),a.end()
+#define sz(a) (int)(a).size()
+#define fi first
+#define se second
+#define rep(i,n) for(int i=0;i<(n);i++)
+#define rep1(i,n) for(int i=1;i<(n);i++)
+#define drep(i,n) for(int i=(n)-1;i>=0;i--)
+#define srep(i,n,s) for(int i=(s);i<(n);i++)
+#define dsrep(i,n,s) for(int i=(n)-1;i>=(s);i--)
+#define pb push_back
+#define eb emplace_back
+#define mp(a,b) make_pair(a,b)
+#define getp(c,x) (lower_bound(c.begin(),c.end(),x)-c.begin())
+#define yes cout<<"Yes"<<endl
+#define YES cout<<"YES"<<endl
+#define no cout<<"No"<<endl
+#define NO cout<<"NO"<<endl
+#define Decimal fixed<<setprecision(20)
+
+using namespace std;
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vc<vc<T>>;
+template<typename T> using pc = pair<T, T>;
+template<typename T> using pq = priority_queue<T, vc<T>, greater<T>>;
+using ll = long long;
+using ld = long double;
+using T3 = tuple<ll,ll,ll>;
+using vll = vc<ll>;
+using vld = vc<ld>;
+using pll = pc<ll>;
+using pld = pc<ld>;
+using vpl = vc<pll>;
+using vs = vc<string>;
+using vvll = vv<ll>;
+using vt = vc<T3>;
+
+
+template<typename T>istream& operator>>(istream&i,vc<T>&v){rep(j,sz(v))i>>v[j];return i;}
+template<typename T>string join(const vc<T>&v,const string& d=""){stringstream s;rep(i,sz(v))(i?s<<d:s)<<v[i];return s.str();}
+template<typename T>ostream& operator<<(ostream&o,const vc<T>&v){if(sz(v))o<<join(v," ");return o;}
+template<typename T1,typename T2>istream& operator>>(istream&i,pair<T1,T2>&v){return i>>v.fi>>v.se;}
+template<typename T1,typename T2>ostream& operator<<(ostream&o,const pair<T1,T2>&v){return o<<v.fi<<","<<v.se;}
+template<typename T>bool mins(T& x,const T&y){if(x>y){x=y;return true;}else return false;}
+template<typename T>bool maxs(T& x,const T&y){if(x<y){x=y;return true;}else return false;}
+template<typename T>ll suma(const vc<T>&a){ll res(0);for(auto&&x:a)res+=x;return res;}
+// template<typename T>void uni(vc<T>& a){sort(all(a));a.erase(unique(rng(a)),a.end());}
+template<typename T>void uni(T& a){sort(all(a));a.erase(unique(all(a)),a.end());}
+
+const int INF = 1001001001;
+const ll LINF = 1LL << 50;
+const double eps = 1e-10;
+const int MOD = 1e9 + 7;
+const int dx[4] = {-1, 0, 1, 0};
+const int dy[4] = {0, -1, 0, 1};
+
+
+vvll a(16,vll(16));
+bool used[16];
+vpl v;
+ll n;
+
+ll dfs(){
+  ll res=0;
+  if(v.size()==n){
+    rep(i,n)res^=a[v[i].fi][v[i].se];
+    return res;
+  }
+
+  ll h;
+  rep(i,2*n){
+    if(!used[i]){
+      h=i;
+      break;
+    }
+  }
+  used[h]=true;
+
+  rep(i,2*n){
+    if(used[i])
+      continue;
+    used[i]=true;
+    pll p=pll(h,i);
+    v.push_back(p);
+    maxs(res,dfs());
+    v.pop_back();
+    used[i]=false;
+  }
+  used[h]=false;
+
+  return res;
+}
+
+
+
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cin>>n;
+  rep(i,2*n){
+    rep(j,2*n){
+      if(j<=i)
+        continue;
+      cin>>a[i][j];
+    }
+  }
+
+  rep(i,16)used[i]=false;
+
+  ll ans=dfs();
+  cout << ans << endl;
+}
+
